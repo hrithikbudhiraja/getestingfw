15084a28cf0cfd02e0f50cc2b05442fc44baefa2
Hrithikbudhiraja1417 <Hrithikbudhiraja1417>
2024-08-10 10:45:38 +0000

    test1

--- Test.py
+++ Test.py
@@ -4,3 +4,21 @@ print("Hello World")
 # COMMAND ----------
 
 # MAGIC  %pip install great-expectations azure-storage-blob
+
+# COMMAND ----------
+
+import great_expectations as ge
+import pandas as pd
+import json
+from datetime import datetime 
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType
+from azure.storage.blob import BlobServiceClient
+import io
+
+# COMMAND ----------
+
+# MAGIC %run /Workspace/Users/hrithikbudhiraja@gmail.com/getestingfw/code/configDetails
+
+# COMMAND ----------
+
+# MAGIC %run /Workspace/Users/hrithikbudhiraja@gmail.com/getestingfw/code/utilities
